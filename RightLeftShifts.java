f93fd206d13515b6b3476481ea9ed14f77630484
AkshayaChalla <akshaya.challa215@gmail.com>
2025-01-29 22:56:14 +0530

    Add files via upload

---
+++ RightLeftShifts.java
@@ -0,0 +1,11 @@
+public class RightLeftShifts {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 55;
+        System.out.println("right shift of a :" + (a<<2));
+        System.out.println("right shift of b :" + (b<<2));
+        System.out.println("left shiftof a :" + (a>>2));
+        System.out.println("left shift of b :" + (b>>2));
+    }
+    
+}
